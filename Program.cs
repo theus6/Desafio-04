730a8bd83ed41c4be59216a6590195dbb050f775
theuz6 <msantossilva005@gmail.com>
2022-09-10 22:54:31 -0300

    main

--- Program.cs
+++ Program.cs
@@ -1,3 +1,9 @@
 ﻿using static System.Console; 
 
+Artigo a1 = new Artigo();
+Artigo a2 = new Artigo();
+Artigo a3 = new Artigo();
+
+List<Autor> autores = new List<Autor>();
+
 Write(" # # # Sistema de Registros de Artigos # # # ");
