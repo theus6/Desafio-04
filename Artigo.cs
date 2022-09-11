5ae9be11f9806347cffa97e8c7592227564607ab
theuz6 <msantossilva005@gmail.com>
2022-09-10 22:54:20 -0300

    New Classes

---
+++ Artigo.cs
@@ -0,0 +1,7 @@
+public class Artigo : Autor
+{
+    public string Titulo {get;set;}
+    public string Palavraschave {get;set;}
+    public string Instituicao {get;set;} 
+    List<Autor> autores; 
+}
---
+++ Autor.cs
@@ -0,0 +1,6 @@
+public class Autor
+{
+    public string Nome {get;set;}
+    public string Email {get;set;}
+        
+}
