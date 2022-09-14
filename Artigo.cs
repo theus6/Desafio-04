08d4f737617fb09d7cfd844e42c2d2199128337d
theuz6 <msantossilva005@gmail.com>
2022-09-13 23:12:32 -0300

    Remoção da lista autores

--- Artigo.cs
+++ Artigo.cs
@@ -3,5 +3,5 @@ public class Artigo : Autor
     public string Titulo {get;set;}
     public string Palavraschave {get;set;}
     public string Instituicao {get;set;} 
-    List<Autor> autores; 
+    
 }
