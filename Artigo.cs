5a8ce35d7efc04b892a1b91c248748c04e8bcf55
theuz6 <msantossilva005@gmail.com>
2022-09-14 10:20:28 -0300

    Lista de autores e metodo de registro dos mesmos

--- Artigo.cs
+++ Artigo.cs
@@ -3,5 +3,15 @@ public class Artigo : Autor
     public string Titulo {get;set;}
     public string Palavraschave {get;set;}
     public string Instituicao {get;set;} 
-    
+    Autor Autor = new Autor();
+
+    public List<Autor> Autores{get;set;} = new List<Autor>();
+
+    public void Registro(Autor autor, string titulo, string instituicao, string palavraschave)
+    {
+        Autores.Add(Autor);
+        this.Titulo = titulo;
+        this.Instituicao = instituicao;
+        this.Palavraschave = palavraschave;
+    }
 }
